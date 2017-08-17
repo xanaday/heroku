207aec3e3be4f8219f0a8e4e8d2a5c4e0667e64c
xanaday <dreamwalker.19@gmail.com>
2017-08-16 20:21:33 -0400

    uncommented

--- models/articles.js
+++ models/articles.js
@@ -3,13 +3,13 @@ const sequelize = require('../db');
 
 const Article = sequelize.define('article', {
     title: { type: Sequelize.STRING, allowNull: false},
-    // author: { type: Sequelize.STRING, allowNull: true},
-    // image: Sequelize.STRING,
+    author: { type: Sequelize.STRING, allowNull: true},
+    image: Sequelize.STRING,
     description: Sequelize.STRING,
     body: Sequelize.STRING,
-    // featured: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false},
-    // showinmenu: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false, field: 'showInMenu'},
-    // published: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false},
+    featured: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false},
+    showinmenu: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false, field: 'showInMenu'},
+    published: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false},
 }, {
     timestamps: false,
 });
